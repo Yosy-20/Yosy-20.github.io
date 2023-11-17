a037c02d0c724b6244f7f57e75a075d96b464e01
Yoseline <123031856+Yosy-20@users.noreply.github.com>
2023-11-17 05:03:22 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: #644b39;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Desarrollo sitios web, proyectos de multimedia y animación digital .</span>')
+  .typeString('<span style="color: #644b39;">Desarrollo sitios web, proyectos de multimedia y Animación digital.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
