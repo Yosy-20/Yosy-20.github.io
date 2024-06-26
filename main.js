4148569d70e826e6c4e89bc2a6a595c72eeef551
Yoseline <123031856+Yosy-20@users.noreply.github.com>
2024-06-25 18:48:38 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// Función para redirigir a sobre mi 
+function irAhero() {
+    window.location.href = "hero.html";
+}
+
+// Función para redirigir a web 
+function irAweb() {
+    window.location.href = "web.html";
+}
+// Función para redirigir a personaje
+function irAcharacter() {
+    window.location.href = "character.html";
+}
+// Función para redirigir a web 
+function irAcontact() {
+    window.location.href = "contact.html";
+}
+
+// Agrega un evento de clic al label
+document.getElementById("aboutMe").addEventListener("click", irAhero);
+
+document.getElementById("webDeveloment").addEventListener("click", irAweb);
+
+document.getElementById("characterDesing").addEventListener("click", irAcharacter);
+
+document.getElementById("contact").addEventListener("click", irAcontact);
+
+//animacion del texto
+let app = document.getElementById('typewriter');
+
+let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+    cursor: "<span style='color: #644b39;'>|</span>",
+});
+
+typewriter
+    .pauseFor(2500)
+    .typeString('<span style="color: #644b39;">Development of websites, web applications, and desktop applications, as well as designing 3D modeled characters and implementing video games.</span>')
+    .pauseFor(200)
+    .deleteChars(10)
+    .start();
+
+    
